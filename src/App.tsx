217c424c909ca1d9013c544e83b49b55818aad3e
mark-pribylnov <mark.pribylnov@gmail.com>
2026-05-02 10:32:16 +0300

    feat: add search functionality with local storage (2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
+import { useState } from 'react';
+import type * as React from 'react';
 import './App.css';
 
+const LOCAL_STORAGE_PROPERTIES = {
+  searches: 'searches',
+};
+
 function App() {
+  const [searchQuery, setSearchQuery] = useState(getLastSearch());
+
+  function onSubmit(event: React.SubmitEvent<HTMLFormElement>): void {
+    event.preventDefault();
+
+    const searches =
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROPERTIES.searches)) ?? [];
+    searches.push(searchQuery);
+
+    localStorage.setItem(
+      LOCAL_STORAGE_PROPERTIES.searches,
+      JSON.stringify(searches)
+    );
+
+    console.log(searches);
+  }
+
+  function getLastSearch(): string {
+    const searches = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_PROPERTIES.searches)
+    );
+    return !searches ? '' : searches.at(-1);
+  }
+
   return (
     <div className="app-container">
       <section className="search-section">
         <h2>Search area:</h2>
-        <input
-          type="text"
-          placeholder="Enter search terms..."
-          className="search-input"
-        />
-        <button className="search-button">Search</button>
+        <form action="#" onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="search-input"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+            aria-label="Search terms"
+          />
+          <button className="search-button">Search</button>
+        </form>
       </section>
 
       <section className="results-section">
